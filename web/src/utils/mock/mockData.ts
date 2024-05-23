4143116de2dd95d2e2f860ff0f0b1443f5301cfe
Adrian Alvarez <adrian.alvarez@xmartcs.com>
2024-05-24 00:58:15 +0200

    mock system implemented

---
+++ web/src/utils/mock/mockData.ts
@@ -0,0 +1,9 @@
+export const mockUsers = [
+    { username: 'user1', password: 'password1', workspace: 'Production' },
+    { username: 'user2', password: 'password2', workspace: 'Testing' },
+];
+
+export const mockWorkspaces = [
+    { Id: 1, Name: 'Production' },
+    { Id: 2, Name: 'Testing' },
+];
